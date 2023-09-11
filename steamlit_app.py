ec6f220bef90288a6f7b3dd5dbd2bbbb889122c3
naveenkuttissery <144649548+naveenkuttissery@users.noreply.github.com>
2023-09-11 10:39:01 +0530

    Update steamlit_app.py
    Breakfast Menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+steamlit.header('Breakfast Menu')
+Steamlit.text('Omega 3 & Blueberry Oatmeal')
+Steamlit.text('Kale, Spinach & Rocket Smoothi')
+Steamlit.text('Hard-Bolid Free-Range Egg')
