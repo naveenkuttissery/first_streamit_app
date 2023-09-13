dec3d89c78b9838cd1a6b57fb87426e339f30acb
naveenkuttissery <144649548+naveenkuttissery@users.noreply.github.com>
2023-09-13 16:19:53 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -72,5 +72,4 @@ if streamlit.button('Add a Fruit to the List'):
 
 fruit_choice = add_my_fruit
 streamlit.write('Thanks for adding : ', fruit_choice)
-streamlit.stop()
-# my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+
