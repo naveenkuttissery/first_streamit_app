d2319e9c66ee36c66f870dd81ee7e00440b9a333
naveenkuttissery <144649548+naveenkuttissery@users.noreply.github.com>
2023-09-13 17:17:48 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -61,8 +61,8 @@ try:
 	
 	def insert_row_snowflake(new_fruit):
 		with my_cnx.cursor as my_cur:
-		   #my_cur.execute("insert into fruit_load_list values('From streamlit 2')")
-		   my_cur.execute("select * from FRUIT_LOAD_LIST")
+		   my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('From streamlit 2')")
+		   #my_cur.execute("select * from FRUIT_LOAD_LIST")
 		   return "Thanks for adding " + new_fruit
 	
 	add_my_fruit = streamlit.text_input('What fruit would you like to add')
